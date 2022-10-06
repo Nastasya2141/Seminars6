d3fec8b620b9bf977e7001969b679ae0556a7331
Nastasya2141 <113186212+Nastasya2141@users.noreply.github.com>
2022-10-06 15:37:21 +0300

    Task41

---
+++ Задача 41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введи число М(количество чисел): ");
+int massiv = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[massiv];
+
+void InputNumbers(int massiv)
+{
+for (int i = 0; i < massiv; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int AmountPositiveNumbers(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if(arr[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(massiv);
+
+Console.WriteLine($"Количество чисел больше 0: {AmountPositiveNumbers(arr)} ");
